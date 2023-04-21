826b8a94f91c8646180ca73b458790f83bc4a1e8
Lucia <lucia.rodriguez.rosales@gmail.com>
2023-04-21 09:18:29 -0300

    Added Login + Nav to App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import Products from './sections/Products';
 import Slider from './components/Slider';
 import {slides} from './utils/constants';
 import About from './sections/About';
+import Nav from './components/Nav';
+import Login from './sections/Login';
 
 function App() {
   return (
     <div>
+      <Nav />
+      <Login />
       <Slider slides={slides} />
       <Products />
       <About/>
