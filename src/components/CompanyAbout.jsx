e3c65bb0b3dff2ce18845f9babf57bcdc4816e10
Lucia <lucia.rodriguez.rosales@gmail.com>
2023-04-21 15:08:48 -0300

    Fixed About components and section

--- src/components/CompanyAbout.jsx
+++ src/components/CompanyAbout.jsx
@@ -1,28 +1,23 @@
 const CompanyAbout = () => (
-    
-    <div className="relative pt-16 bg-blueGray-50 ">
-    <div className="container mx-auto">
-      <div className="flex flex-wrap items-center">
-        <div className="w-10/12 md:w-6/12 lg:w-5/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
-          <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-hergo-red">
-            <img alt="..." src="https://media.discordapp.net/attachments/1097492856966479993/1098672652111061152/Hergo.jpg?width=480&height=480" className="w-full h-auto rounded-t-lg" />
-            <blockquote className="relative p-8 mb-4">
-              <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" className="absolute left-0 w-full block h-95-px -top-94-px">
-              </svg>
-              <h2 className="font-bold text-white text-lg" >
-              Su aliado mayorista de confianza: Hergo.
-              </h2>
-              <p className="text-md font-light mt-2 text-white">
-              Somos una empresa marplatense dedicada a la venta Mayorista y distribución de productos de consumo masivo.
-              Con una trayectoria de 25 años que avalan nuestra experiencia y una excelente capacidad operativa, trabajamos para cubrir las necesidades del mercado local y zonal, 
-              siempre bajo el imperativo de ofrecer el más amplio surtido de productos de primer nivel al mejor precio.
-              </p>
-            </blockquote>
-          </div>
-        </div>
-      </div>
+  <div className="relative flex flex-col w-full mb-6 shadow-lg rounded-lg bg-hergo-red">
+    <img
+      src="https://media.discordapp.net/attachments/1097492856966479993/1098672652111061152/Hergo.jpg?width=480&height=480"
+      className="w-full h-[25em] object-cover object-center rounded-t-lg"
+    />
+    <div className="relative p-5 mb-4">
+      <h2 className="font-extrabold text-white text-[2em] leading-tight">
+        Su aliado mayorista de confianza
+      </h2>
+      <p className="text-md mt-2 text-white">
+        Somos una empresa marplatense dedicada a la venta Mayorista y
+        distribución de productos de consumo masivo. Con una trayectoria de 25
+        años que avala nuestra experiencia y una excelente capacidad operativa,
+        trabajamos para cubrir las necesidades del mercado local y zonal,
+        siempre bajo el imperativo de ofrecer el más amplio surtido de productos
+        de primer nivel al mejor precio.
+      </p>
     </div>
-    </div>
-)
+  </div>
+);
 
 export default CompanyAbout;
